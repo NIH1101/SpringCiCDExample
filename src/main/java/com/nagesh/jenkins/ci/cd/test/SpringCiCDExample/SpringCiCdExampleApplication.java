2a0e9d4f1e44b8c7d1231a3ef3d719c03fe50d68
Nagesh Hawanna <hawannanagesh@yahoo.in>
2019-08-10 12:49:12 +0530

    First Commit

---
+++ src/main/java/com/nagesh/jenkins/ci/cd/test/SpringCiCDExample/SpringCiCdExampleApplication.java
@@ -0,0 +1,13 @@
+package com.nagesh.jenkins.ci.cd.test.SpringCiCDExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCiCdExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCiCdExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagesh/jenkins/ci/cd/test/SpringCiCDExample/SpringCiCdExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nagesh.jenkins.ci.cd.test.SpringCiCDExample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCiCdExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
